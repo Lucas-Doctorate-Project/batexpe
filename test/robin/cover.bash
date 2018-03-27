793c775002f1d0e29f56a8db42061ac537579c95
Millian Poquet <millian.poquet@inria.fr>
2018-03-27 20:48:38 +0200

    [coverage] generate distinct bats files

--- test/robin/cover.bash
+++ test/robin/cover.bash
@@ -1,24 +1,29 @@
 #!/usr/bin/env bash
 set -eux
 
+# Create a copy of each bats file
+rm -f *-cover.bats
+find . -name '*.bats' | \
+    sed 'sW\(\(.*\)/\(.*\)\.bats\)Wcp -f \1 \2/\3-cover.batsW' | bash -x
+
 # This command replaces "robintest" by "robintest --cover=AB.covout"
 # in each of the .bats file of the current directory, where:
 # - A is the name of the .bats file
 # - B is an increasing number
 #
 # This way, the coverage of each bats test is written to a unique file
-find . -name '*.bats' | \
+find . -name '*-cover.bats' | \
     sed 'sW\(\./\(.*\)\.bats\)Wawk -i inplace '\''{for(x=1;x<=NF;x++)if($x~/robintest/){sub(/robintest/,"robintest --cover=\2"++i".covout")}}1'\'' \1W' | \
     bash -x
 
+# Clean previous coverage results if needed
+rm -f *.covout coverage-report.txt
+
 # Run the tests, so coverage files can be obtained
-bats . || true
+bats *-cover.bats || true
 
 # Merge all coverage files into one
 gocovmerge *.covout > merged.covout
 
 # Get a readable coverage report
 gocov convert merged.covout | gocov report > coverage-report.txt
-
-# Revert .bats files to their previous state (if in a git repo)
-git checkout -- *.bats || true
