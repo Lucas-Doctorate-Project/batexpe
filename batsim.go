62754c45f34d9bff03d036034e658b855af01feb
Millian Poquet <millian.poquet@inria.fr>
2018-11-16 19:57:04 +0100

    [code] use batsim --dump-execution-context
    This replaces the old behaviour of parsing the Batsim CLI in batexpe.
    
    This allows Batsim to update its CLI without requiring batexpe to adapt
    a lot. Adding new options to Batsim's CLI should not require any modification
    here.

--- batsim.go
+++ batsim.go
@@ -1,10 +1,12 @@
 package batexpe
 
 import (
+	"encoding/json"
 	"fmt"
-	"github.com/anmitsu/go-shlex"
-	docopt "github.com/docopt/docopt-go"
 	log "github.com/sirupsen/logrus"
+	"io/ioutil"
+	"os"
+	"os/exec"
 )
 
 type BatsimArgs struct {
@@ -14,132 +16,44 @@ type BatsimArgs struct {
 }
 
 func ParseBatsimCommand(batcmd string) (batargs BatsimArgs, err error) {
-	batsimDocopt := `
-A tool to simulate (via SimGrid) the behaviour of scheduling algorithms.
+	// Goal: Run batsim with --dump-execution-context and parse its JSON output.
+	executedCmd := batcmd + " --dump-execution-context"
 
-Usage:
-  batsim -p <platform_file> [-w <workload_file>...]
-                            [-W <workflow_file>...]
-                            [--WS (<cut_workflow_file> <start_time>)...]
-                            [options]
-  batsim --help
-  batsim --version
-  batsim --simgrid-version
-  batsim --unittest
-
-Input options:
-  -p --platform <platform_file>     The SimGrid platform to simulate.
-  -w --workload <workload_file>     The workload JSON files to simulate.
-  -W --workflow <workflow_file>     The workflow XML files to simulate.
-  --WS --workflow-start (<cut_workflow_file> <start_time>)... The workflow XML
-                                    files to simulate, with the time at which
-                                    they should be started.
-
-Most common options:
-  -m, --master-host <name>          The name of the host in <platform_file>
-                                    which will be used as the RJMS management
-                                    host (thus NOT used to compute jobs)
-                                    [default: master_host].
-  -E --energy                       Enables the SimGrid energy plugin and
-                                    outputs energy-related files.
-
-Execution context options:
-  --config-file <cfg_file>          Configuration file name (optional). [default: None]
-  -s, --socket-endpoint <endpoint>  The Decision process socket endpoint
-                                    Decision process [default: tcp://localhost:28000].
-  --redis-hostname <redis_host>     The Redis server hostname. Read from config file by default.
-                                    [default: None]
-  --redis-port <redis_port>         The Redis server port. Read from config file by default.
-                                    [default: -1]
-  --redis-prefix <prefix>           The Redis prefix. Read from config file by default.
-                                    [default: None]
-
-Output options:
-  -e, --export <prefix>             The export filename prefix used to generate
-                                    simulation output [default: out].
-  --enable-sg-process-tracing       Enables SimGrid process tracing
-  --disable-schedule-tracing        Disables the Pajé schedule outputting.
-  --disable-machine-state-tracing   Disables the machine state outputting.
-
-
-Platform size limit options:
-  --mmax <nb>                       Limits the number of machines to <nb>.
-                                    0 means no limit [default: 0].
-  --mmax-workload                   If set, limits the number of machines to
-                                    the 'nb_res' field of the input workloads.
-                                    If several workloads are used, the maximum
-                                    of these fields is kept.
-Verbosity options:
-  -v, --verbosity <verbosity_level> Sets the Batsim verbosity level. Available
-                                    values: quiet, network-only, information,
-                                    debug [default: information].
-  -q, --quiet                       Shortcut for --verbosity quiet
-
-Workflow options:
-  --workflow-jobs-limit <job_limit> Limits the number of possible concurrent
-                                    jobs for workflows. 0 means no limit
-                                    [default: 0].
-  --ignore-beyond-last-workflow     Ignores workload jobs that occur after all
-                                    workflows have completed.
-
-Other options:
-  --allow-time-sharing              Allows time sharing: One resource may
-                                    compute several jobs at the same time.
-  --batexec                         If set, the jobs in the workloads are
-                                    computed one by one, one after the other,
-                                    without scheduler nor Redis.
-  --pfs-host <pfs_host>             The name of the host, in <platform_file>,
-                                    which will be the parallel filesystem target
-                                    as data sink/source for the large-capacity
-                                    storage tier [default: pfs_host].
-  --hpst-host <hpst_host>           The name of the host, in <platform_file>,
-                                    which will be the parallel filesystem target
-                                    as data sink/source for the high-performance
-                                    storage tier [default: hpst_host].
-  -h --help                         Shows this help.
-`
-
-	splitCmd, err := shlex.Split(batcmd, true)
+	// Create a temporary command file for the command to run.
+	cmdFile, err := ioutil.TempFile(".", "tmp-dump-batcmd")
 	if err != nil {
-		return batargs, fmt.Errorf("Cannot split Batsim command (err=%s)",
-			err.Error())
+		return batargs, fmt.Errorf("Cannot create temporary file")
+	}
+	defer os.Remove(cmdFile.Name())
+
+	// Dump command to the temporary file.
+	cmdFile.Write([]byte(executedCmd))
+
+	cmd := exec.Command("bash")
+	cmd.Args = []string{cmd.Args[0], "-eux", cmdFile.Name()}
+
+	log.WithFields(log.Fields{
+		"command": executedCmd,
+	}).Debug("Executing Batsim to parse its arguments")
+
+	out, err := cmd.Output()
+	if err != nil {
+		return batargs, err
 	}
 
 	log.WithFields(log.Fields{
-		"batsim command": batcmd,
-	}).Debug("Batcmd -> shlex -> split")
+		"output": string(out),
+	}).Debug("Batsim execution was successful (to parse its arguments)")
 
-	if len(splitCmd) <= 1 {
-		return batargs, fmt.Errorf("Batsim command should have at least 2 parts")
+	// Parse output JSON to know the command execution context
+	var jsonData map[string]interface{}
+	if err := json.Unmarshal(out, &jsonData); err != nil {
+		return batargs, fmt.Errorf("Could not parse Batsim (expected to be JSON) output")
 	}
 
-	arguments, err := docopt.Parse(batsimDocopt, splitCmd[1:], true, "?",
-		false, false)
-	if err != nil {
-		return batargs, fmt.Errorf("Cannot parse Batsim command (err=%s)", err.Error())
-	}
-
-	batargs.Socket = "tcp://localhost:28000"
-	batargs.ExportPrefix = "out"
-	batargs.BatexecMode = false
-
-	if arguments["--socket-endpoint"] != nil {
-		batargs.Socket = arguments["--socket-endpoint"].(string)
-	}
-
-	if arguments["--export"] != nil {
-		batargs.ExportPrefix = arguments["--export"].(string)
-	}
-
-	if arguments["--batexec"] != nil {
-		batargs.BatexecMode = arguments["--batexec"].(bool)
-	}
-
-	log.WithFields(log.Fields{
-		"socket":        batargs.Socket,
-		"export prefix": batargs.ExportPrefix,
-		"batexec?":      batargs.BatexecMode,
-	}).Debug("Parsed Batsim command")
+	batargs.Socket = jsonData["socket_endpoint"].(string)
+	batargs.ExportPrefix = jsonData["export_prefix"].(string)
+	batargs.BatexecMode = !jsonData["external_scheduler"].(bool)
 
 	return batargs, nil
 }
